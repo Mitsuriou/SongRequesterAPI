4105a5857b5a94cd754aff3b58b398e432408c46
Mitsuriou <dylanpro.jacquemin@gmail.com>
2021-01-09 01:57:15 +0100

    Basic setup to take into account the database. API is now capable to execute requests and send back answers. Remove MariaDB package. Installed Nodemon package.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# SongRequesterAPI
+# Song Requester API
+## NodeJS API layer for the Song Requester project
+
+Find the web project [here](https://github.com/Mitsuriou/SongRequester).  
+Find the database [here](https://github.com/Mitsuriou/SongRequesterDatabase).
+
+### ROADMAP
+- [TO COMPLETE]
+- Unit tests
--- requestDAO.js
+++ requestDAO.js
@@ -1,73 +1,22 @@
+// Modules import
 const postgresql = require('pg');
-const identifiants = require('./connection_strings.js');
 
-var chaineDeConnexion = 'postgres://' + identifiants.identifiant + ':' + identifiants.motDePasse
-    +'@' + identifiants.ip + ':' + identifiants.port + '/' + identifiants.baseDeDonnees;
+// Local files import
+const IDENTIFIERS = require('./database_identifiers.js');
 
-const SQL_LISTE_TEMPERATURE = 'SELECT * FROM brute_archivage LIMIT 200;';
-const SQL_MOYENNE_TEMPERATURE = 'SELECT moyenne_temperature, timestamp FROM vue ORDER BY ID DESC LIMIT 1;';
-const SQL_MEDIANE_TEMPERATURE = 'SELECT * FROM brute_archivage ORDER BY temperature OFFSET ((SELECT count(*) FROM brute_archivage) / 2) LIMIT 1;';
-const SQL_MINIMUM_TEMPERATURE = 'SELECT * FROM brute_archivage WHERE temperature = (SELECT MIN(temperature) FROM brute_archivage);';
-const SQL_MAXIMUM_TEMPERATURE = 'SELECT * FROM brute_archivage WHERE temperature = (SELECT MAX(temperature) FROM brute_archivage);';
-const SQL_MODE_TEMPERATURE = "SELECT COUNT(*) as compte, temperature FROM brute_archivage GROUP BY brute_archivage.temperature ORDER BY compte DESC LIMIT 1;";
+// Global constants and variables
+const CONNECTION_STRING = 'postgres://' + IDENTIFIERS.id + ':' + IDENTIFIERS.password
+    +'@' + IDENTIFIERS.ip + ':' + IDENTIFIERS.port + '/' + IDENTIFIERS.database;
 
-exports.listerTemperatures = async function listerTemperatures() {
-    const basededonnees = new postgresql.Client(chaineDeConnexion);
+// SQL requests
+const SQL_BASIC_REQUEST = "SELECT * FROM sr.user;";
 
-    await basededonnees.connect();
-    var listeTemperature = await basededonnees.query(SQL_LISTE_TEMPERATURE);
-    await basededonnees.end();
+exports.listUsers = async function() {
+    const lDatabase = new postgresql.Client(CONNECTION_STRING);
 
-    return listeTemperature.rows;
-};
+    await lDatabase.connect();
+    var lUsersList = await lDatabase.query(SQL_BASIC_REQUEST);
+    await lDatabase.end();
 
-exports.moyenneTemperature = async function moyenneTemperature(){
-    const basededonnees = new postgresql.Client(chaineDeConnexion);
-
-    await basededonnees.connect();
-    var moyTemp = await basededonnees.query(SQL_MOYENNE_TEMPERATURE);
-    await basededonnees.end();
-
-    return moyTemp.rows;
-};
-
-exports.medianeTemperature = async function medianeTemperature(){
-    const basededonnees = new postgresql.Client(chaineDeConnexion);
-
-    await basededonnees.connect();
-    var medTemp = await basededonnees.query(SQL_MEDIANE_TEMPERATURE);
-    await basededonnees.end();
-
-    return medTemp.rows;
-};
-
-exports.minimumTemperature = async function minimumTemperature(){
-    const basededonnees = new postgresql.Client(chaineDeConnexion);
-
-    await basededonnees.connect();
-    var minTemp = await basededonnees.query(SQL_MINIMUM_TEMPERATURE);
-    await basededonnees.end();
-
-    return minTemp.rows;
-};
-
-exports.maximumTemperature = async function maximumTemperature(){
-    const basededonnees = new postgresql.Client(chaineDeConnexion);
-
-    await basededonnees.connect();
-    var maxTemp = await basededonnees.query(SQL_MAXIMUM_TEMPERATURE);
-    await basededonnees.end();
-
-    return maxTemp.rows;
-};
-
-exports.modeTemperature = async function modeTemperature(){
-    const basededonnees = new postgresql.Client(chaineDeConnexion);
-
-    await basededonnees.connect();
-    var modTemp = await basededonnees.query(SQL_MODE_TEMPERATURE);
-    await basededonnees.end();
-
-    return modTemp.rows;
-
+    return lUsersList.rows;
 };
