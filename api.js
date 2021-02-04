a671ca883a3c7149bf7adf5808f8e8b6a30cac34
Mitsuriou <dylanpro.jacquemin@gmail.com>
2021-02-05 00:08:14 +0100

    Small progress + VSCode JSON launch file

--- api.js
+++ api.js
@@ -5,12 +5,18 @@ const app = express();
 // Local files import
 const requestDAO = require('./requestDAO');
 
-app.get('/user', async (request, answer) => {
+app.get('/users', async (request, answer) => {
   try {
-    const lUsersList = await requestDAO.listUsers();
-    answer.status(200).json(lUsersList);
+    answer.status(200).json(await requestDAO.allUsers());
   } catch (error) {
-    console.log(error);
+    answer.status(500).json(error.message);
+  }
+});
+
+app.get('/user/:id', async (request, answer) => {
+  try {
+    answer.status(200).json(await requestDAO.getUser(request.params.id));
+  } catch (error) {
     answer.status(500).json(error.message);
   }
 });
--- requestDAO.js
+++ requestDAO.js
@@ -4,11 +4,8 @@ const postgresql = require('pg');
 // Local files import
 const IDENTIFIERS = require('./database_identifiers.js');
 
-// SQL requests
-const SQL_BASIC_REQUEST = 'SELECT * FROM sr.user;';
-
 // Exported functions (ASYNC calls)
-exports.listUsers = async function () {
+exports.allUsers = async function () {
   const lDatabase = new postgresql.Client({
     user: IDENTIFIERS.id,
     password: IDENTIFIERS.password,
@@ -18,8 +15,27 @@ exports.listUsers = async function () {
     database: IDENTIFIERS.database,
   });
 
+  const SQL_REQUEST = 'SELECT * FROM sr.user;';
   await lDatabase.connect();
-  var lUsersList = await lDatabase.query(SQL_BASIC_REQUEST);
+  var lUsersList = await lDatabase.query(SQL_REQUEST);
+  await lDatabase.end();
+
+  return lUsersList.rows;
+};
+
+exports.getUser = async function (id) {
+  const lDatabase = new postgresql.Client({
+    user: IDENTIFIERS.id,
+    password: IDENTIFIERS.password,
+    host: IDENTIFIERS.ip,
+    port: IDENTIFIERS.port,
+    client_encoding: 'UTF8',
+    database: IDENTIFIERS.database,
+  });
+
+  const SQL_REQUEST = `SELECT * FROM sr.user where sr.user.id = ${id};`;
+  await lDatabase.connect();
+  var lUsersList = await lDatabase.query(SQL_REQUEST);
   await lDatabase.end();
 
   return lUsersList.rows;
